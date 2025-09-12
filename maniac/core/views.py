0d7504d6969b86a6ff4f81394bc97c35afcf51a9
darkcom109 <garciavalentino2006@gmail.com>
2025-09-13 00:05:28 +0100

    Added views and html files

--- maniac/core/views.py
+++ maniac/core/views.py
@@ -1,6 +1,19 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 
-def home(request):
-    return HttpResponse("This is a test")
+def feed(request):
+    return render(request, 'core/feed.html')
+
+def profile(request, username):
+    return render(request, 'core/profile.html', {'username': username})
+
+def login_view(request):
+    return render(request, 'core/login.html')
+
+def signup_view(request):
+    return render(request, 'core/signup.html')
+
+def post_view(request):
+    return render(request, 'core/post.html')
