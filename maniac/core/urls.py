abe6fb192dc51eb452a12816e563799d84834f4f
darkcom109 <garciavalentino2006@gmail.com>
2025-09-13 14:28:18 +0100

    Added signup and login pages, templates and authentication

--- maniac/core/urls.py
+++ maniac/core/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.feed, name='feed'),
     path('signup/', views.signup_view, name='signup'),
+    path('login/', views.login_view, name='login'),
     path('profile/<str:username>', views.profile, name='profile'),
     path('login/', views.login_view, name='login'),
     path('signup/', views.signup_view, name='signup'),
--- maniac/core/views.py
+++ maniac/core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
 from django.http import HttpResponse
 
 # Create your views here.
@@ -21,11 +22,22 @@ def signup_view(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            print("USER ADDED TO DB")
             return HttpResponse("Signup Successful!")
         else:
-            print("USER WAS NOT ADDED TO DB")
             return HttpResponse(f"Form errors: {form.errors}")
     else:
         form = UserCreationForm()
     return render(request, 'core/signup.html', {'form' : form})
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return HttpResponse(f"Welcome back, {user.username}!")
+        else: 
+            return HttpResponse(f"Login failed. Errors: {form.errors}")
+    else:
+        form = AuthenticationForm()
+    return render(request, 'core/login.html', {'form' : form})
